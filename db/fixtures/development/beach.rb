16764ceb06744333ebb10864b388bc5df3c19198
Yuki Matsuyama <matsumatsu20@gmail.com>
2016-05-19 23:53:18 +0900

    初期データを追加した

--- db/fixtures/development/beach.rb
+++ db/fixtures/development/beach.rb
@@ -7,3 +7,24 @@ Beach.seed do |b|
   b.longitude   = 139.766076
   b.parking     = "有（有料）"
 end
+
+Beach.seed do |b|
+  b.id          = 2
+  b.name        = "悲惨な海岸"
+  b.description = "クオリティ低めです"
+  b.address     = "東京都立川市曙町"
+  b.latitude    = 35.681368
+  b.longitude   = 139.766076
+  b.parking     = "有（有料）"
+end
+
+
+Beach.seed do |b|
+  b.id          = 3
+  b.name        = "スペシャル海岸"
+  b.description = "ロケーション最高"
+  b.address     = "神奈川県横浜市海の見える丘公園"
+  b.latitude    = 35.681368
+  b.longitude   = 139.766076
+  b.parking     = "有（有料）"
+end
