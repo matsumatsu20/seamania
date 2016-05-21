59c2696b3b2e76d88cab24e5c58bd125bba8f1bf
Yuki Matsuyama <matsumatsu20@gmail.com>
2016-05-21 18:14:45 +0900

    本番データを挿入

--- db/fixtures/development/beach.rb
+++ db/fixtures/development/beach.rb
@@ -10,21 +10,21 @@ end
 
 Beach.seed do |b|
   b.id          = 2
-  b.name        = "悲惨な海岸"
-  b.description = "クオリティ低めです"
-  b.address     = "東京都立川市曙町"
-  b.latitude    = 35.681368
-  b.longitude   = 139.766076
+  b.name        = "白浜大浜海水浴場"
+  b.description = "静岡県下田市に位置する海水浴場の一つ。水質が非常によく、またその名の通り美しい白砂が特徴的。若者が多く集まり、海水浴のシーズンは多くの海水浴客やサーファーが全国から集まる"
+  b.address     = "静岡県下田市白浜"
+  b.latitude    = 34.6909481
+  b.longitude   = 138.9730694
   b.parking     = "有（有料）"
 end
 
 
 Beach.seed do |b|
   b.id          = 3
-  b.name        = "スペシャル海岸"
-  b.description = "ロケーション最高"
-  b.address     = "神奈川県横浜市海の見える丘公園"
-  b.latitude    = 35.681368
-  b.longitude   = 139.766076
+  b.name        = "銚子マリーナ海水浴場"
+  b.description = "東洋のドーバーと言われる屏風ケ浦を前面に眺めながらの波の静かな海水浴場。落ち着いた雰囲気で小さなお子様連れのご家族にもオススメの海水浴場です。"
+  b.address     = "千葉県銚子市若宮町１-１"
+  b.latitude    = 35.6992909
+  b.longitude   = 140.8312433
   b.parking     = "有（有料）"
 end
