7fc312d252f573bc4a78605de5a76b73d980af2b
Yuki Matsuyama <matsumatsu20@gmail.com>
2016-06-13 01:19:46 +0900

    データを追加

--- db/fixtures/beaches_tag.rb
+++ db/fixtures/beaches_tag.rb
@@ -2,10 +2,200 @@ BeachesTag.delete_all
 
 BeachesTag.create(
   beach_id: 1,
-  tag_id:   1
+  tag_id:   3
 )
 
 BeachesTag.create(
   beach_id: 1,
+  tag_id:   4
+)
+
+BeachesTag.create(
+  beach_id: 2,
   tag_id:   2
+)
+
+BeachesTag.create(
+  beach_id: 2,
+  tag_id:   7
+)
+
+BeachesTag.create(
+  beach_id: 3,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 4,
+  tag_id:   3
+)
+
+BeachesTag.create(
+  beach_id: 4,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 5,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 6,
+  tag_id:   2
+)
+
+BeachesTag.create(
+  beach_id: 7,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 7,
+  tag_id:   2
+)
+
+BeachesTag.create(
+  beach_id: 7,
+  tag_id:   7
+)
+
+BeachesTag.create(
+  beach_id: 8,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 8,
+  tag_id:   4
+)
+
+BeachesTag.create(
+  beach_id: 9,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 9,
+  tag_id:   3
+)
+
+BeachesTag.create(
+  beach_id: 9,
+  tag_id:   4
+)
+
+BeachesTag.create(
+  beach_id: 10,
+  tag_id:   2
+)
+
+BeachesTag.create(
+  beach_id: 10,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 10,
+  tag_id:   4
+)
+
+BeachesTag.create(
+  beach_id: 11,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 11,
+  tag_id:   3
+)
+
+BeachesTag.create(
+  beach_id: 12,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 12,
+  tag_id:   5
+)
+
+BeachesTag.create(
+  beach_id: 13,
+  tag_id:   2
+)
+
+BeachesTag.create(
+  beach_id: 14,
+  tag_id:   2
+)
+
+BeachesTag.create(
+  beach_id: 14,
+  tag_id:   3
+)
+
+BeachesTag.create(
+  beach_id: 15,
+  tag_id:   2
+)
+
+BeachesTag.create(
+  beach_id: 16,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 16,
+  tag_id:   8
+)
+
+BeachesTag.create(
+  beach_id: 16,
+  tag_id:   8
+)
+
+BeachesTag.create(
+  beach_id: 17,
+  tag_id:   7
+)
+
+BeachesTag.create(
+  beach_id: 18,
+  tag_id:   3
+)
+
+BeachesTag.create(
+  beach_id: 19,
+  tag_id:   3
+)
+
+BeachesTag.create(
+  beach_id: 20,
+  tag_id:   3
+)
+
+BeachesTag.create(
+  beach_id: 20,
+  tag_id:   7
+)
+
+BeachesTag.create(
+  beach_id: 21,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 21,
+  tag_id:   4
+)
+
+BeachesTag.create(
+  beach_id: 22,
+  tag_id:   1
+)
+
+BeachesTag.create(
+  beach_id: 22,
+  tag_id:   4
 )
--- db/fixtures/tag.rb
+++ db/fixtures/tag.rb
@@ -1,6 +1,6 @@
 Tag.seed do |t|
   t.id   = 1
-  t.name = "ファミリー向け"
+  t.name = "家族におすすめ"
 end
 
 Tag.seed do |t|
@@ -10,15 +10,30 @@ end
 
 Tag.seed do |t|
   t.id   = 3
-  t.name = "年齢層若め"
+  t.name = "若者におすすめ"
 end
 
 Tag.seed do |t|
   t.id   = 4
-  t.name = "音楽禁止"
+  t.name = "カップルにおすすめ"
 end
 
 Tag.seed do |t|
   t.id   = 5
-  t.name = "お酒が飲める"
+  t.name = "夜景も楽しめる"
+end
+
+Tag.seed do |t|
+  t.id   = 6
+  t.name = "磯遊びができる"
+end
+
+Tag.seed do |t|
+  t.id   = 7
+  t.name = "サーフィンにおすすめ"
+end
+
+Tag.seed do |t|
+  t.id   = 8
+  t.name = "ダイビング"
 end
