a92e7560d2c77a91057fe486e7a709d07b60e2dd
Yuki Matsuyama <matsumatsu20@gmail.com>
2016-06-13 01:19:23 +0900

    タグ機能

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,11 +1,16 @@
 class BeachesController < ApplicationController
   before_action :set_prefecture, only: %i(index)
+  before_action :set_tag, only: %i(index)
 
   def index
     @beaches = if @prefecture.present?
       @prefecture.beaches.page(params[:page])
     else
-      Beach.all
+      Beach.all.page(params[:page])
+    end
+
+    if params[:tag_id].present?
+      @beaches = @beaches.joins(:tags).where(tags: {id: @tag}).page(params[:page])
     end
   end
 
@@ -21,5 +26,9 @@ class BeachesController < ApplicationController
 
   def set_prefecture
     @prefecture = Prefecture.find(params[:prefecture_id]) if params[:prefecture_id].present?
+  end
+
+  def set_tag
+    @tag = Tag.find(params[:tag_id]) if params[:tag_id].present?
   end
 end
